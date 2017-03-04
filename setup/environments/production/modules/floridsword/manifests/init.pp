7072a95eae4e5860db6141cbe57089b8f501c987
David A. Sjøen <dasjoen@gmail.com>
2017-03-04 21:45:46 +0100

    Added dotnet install

--- setup/environments/production/modules/floridsword/manifests/init.pp
+++ setup/environments/production/modules/floridsword/manifests/init.pp
@@ -10,4 +10,31 @@ class floridsword {
         ensure => present,
         source => 'puppet:///modules/floridsword/local-dns',
     }
+
+    package { 'curl':
+        ensure => installed,        
+    }
+
+    package { 'libunwind8':
+        ensure => installed,        
+    }
+
+    package { 'gettext':
+        ensure => installed,        
+    }
+
+    file { '/opt/dotnet':
+        ensure => directory,
+    }
+    ->
+    exec { 'install_dotnet':
+        command => "/usr/bin/curl -sSL https://go.microsoft.com/fwlink/?LinkID=835021 | /bin/tar zx -C /opt/dotnet",
+        creates => "/opt/dotnet/dotnet",
+        require => [ Package['curl'], Package['libunwind8'], Package['gettext'] ],
+    }
+    ->
+    file { '/usr/local/bin/dotnet':
+        ensure => link,
+        target => '/opt/dotnet/dotnet',
+    }
 }
